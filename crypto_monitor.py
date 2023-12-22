f354e30660fdfd0a64615cee8a5804ed056dd8a9
jami100YT <s.zawarty1983@gmail.com>
2023-12-22 16:42:48 +0100

    more data about the crypto coin

--- crypto_monitor.py
+++ crypto_monitor.py
@@ -1,7 +1,7 @@
 import time
 import requests
 import mysql.connector
-import datetime
+from datetime import datetime
 from yaml import safe_load
 
 class CryptoMonitor:
@@ -21,7 +21,25 @@ class CryptoMonitor:
             CREATE TABLE IF NOT EXISTS {coin}_data (
                 id INT AUTO_INCREMENT PRIMARY KEY,
                 timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                price_eur DECIMAL(10, 5)
+                price_eur DECIMAL(10, 5),
+                market_cap DECIMAL(20, 5),
+                volume DECIMAL(20, 5),
+                high_24h DECIMAL(10, 5),
+                low_24h DECIMAL(10, 5),
+                price_change_24h DECIMAL(10, 5),
+                price_change_percentage_24h DECIMAL(10, 5),
+                market_cap_change_24h DECIMAL(20, 5),
+                market_cap_change_percentage_24h DECIMAL(10, 5),
+                circulating_supply DECIMAL(20, 5),
+                total_supply DECIMAL(20, 5),
+                max_supply DECIMAL(20, 5),
+                ath DECIMAL(10, 5),
+                ath_change_percentage DECIMAL(10, 5),
+                atl DECIMAL(10, 5),
+                atl_change_percentage DECIMAL(10, 5),
+                last_updated TIMESTAMP,
+                symbol TEXT,
+                image TEXT
             );
         '''
         self.cursor.execute(create_table_sql)
@@ -29,32 +47,49 @@ class CryptoMonitor:
 
     def load_config(self):
         with open(self.config_path, "r") as yaml_file:
-            config = safe_load(yaml_file)
+            config: dict = safe_load(yaml_file)
         return config
 
     def fetch_crypto_data(self):
         coins = self.load_config().get("COINS")
         api_url = self.api_url_base.replace("<COINS>", coins)
         response = requests.get(api_url)
-        data = response.json()
-
-        # Überprüfe den API-Status
-        status = data.get("status", None)
-        if status and status.get("error_code") == 429:
-            print("API Rate Limit überschritten. Bitte warte, bevor du erneut abrufst.")
-            time.sleep(180)
-            return None
-        elif data.get("error") or data == {}:
-            print(f"Request has failded: {data.get('error')}")
-            return None
+        data: list[dict] = response.json()
+        
+        if(type(data) == dict and data.get("status")):
+            print(f'error code: {data.get("status").get("error_code")}\nerror_essage: {data.get("status").get("error_message")}')
+            if(data.get("status").get("error_message") == 429):
+                time.sleep(180)
+                return 429
+        elif (type(data) == dict and data.get("error")):
+            print(data.get("error"))
+            return None 
         else:
-            print(f"[{datetime.datetime.now().strftime('%d.%m.%Y %H:%M:%S')}] Request wurde erfolgreich durchgeführt!")
-        return data
+            print(f"[{datetime.now().strftime('%d.%m.%Y %H:%M:%S')}] Request wurde erfolgreich durchgeführt!")
+            return data 
 
-    def insert_data_into_database(self, coin, price_eur):
+
+    def insert_data_into_database(self, coin, details):
         self.create_table_if_not_exists(coin)
-        insert_sql = f"INSERT INTO {coin}_data (price_eur) VALUES (%s)"
-        values = (price_eur,)
+        insert_sql = f'''
+            INSERT INTO {coin}_data (
+                price_eur, market_cap, volume, high_24h, low_24h, 
+                price_change_24h, price_change_percentage_24h, 
+                market_cap_change_24h, market_cap_change_percentage_24h,
+                circulating_supply, total_supply, max_supply,
+                ath, ath_change_percentage, atl, atl_change_percentage,
+                last_updated, symbol, image
+            ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+        '''
+        values = (
+            details['current_price'], details['market_cap'], details['total_volume'],
+            details['high_24h'], details['low_24h'], details['price_change_24h'],
+            details['price_change_percentage_24h'], details['market_cap_change_24h'],
+            details['market_cap_change_percentage_24h'], details['circulating_supply'],
+            details['total_supply'], details['max_supply'], details['ath'],
+            details['ath_change_percentage'], details['atl'], details['atl_change_percentage'],
+            datetime.strptime(details['last_updated'], '%Y-%m-%dT%H:%M:%S.%fZ'), details["symbol"], details["image"]
+        )
         self.cursor.execute(insert_sql, values)
         self.db_connection.commit()
 
@@ -63,11 +98,13 @@ class CryptoMonitor:
             try:
                 data = self.fetch_crypto_data()
 
-                if data is not None:
-                    for coin, details in data.items():
-                        price_eur = details['eur']
-                        self.insert_data_into_database(coin, price_eur)
-                        #print(f"Daten für {coin} erfolgreich in die Datenbank eingefügt.")
+                if data is not None and data != 429:
+                    for coin in data:
+                        self.insert_data_into_database(coin.get("id"), coin)
+                        print(f"Daten für {coin['name']} erfolgreich in die Datenbank eingefügt.")
+
+                else:
+                    time.sleep(180)
             except Exception as e:
                 print(f"Fehler beim Abrufen und Speichern der Daten: {e}")
 
